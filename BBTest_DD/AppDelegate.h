54a4b172583dadd567f7428229851a630c0bdf08
Banbao12 <Mibao12@icloud.com>
2021-04-14 09:54:35 +0800

    mmmmdfd

---
+++ BBTest_DD/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BBTest_DD
+//
+//  Created by 半饱 on 2021/4/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BBTest_DD/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  BBTest_DD
+//
+//  Created by 半饱 on 2021/4/14.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
